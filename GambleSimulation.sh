8fc802ae88f4c0d5fe612c06715f7f8a67616c32
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-10 19:45:28 +0530

    [Vaidehi]ADD:WinningLoosingCondition

--- GambleSimulation.sh
+++ GambleSimulation.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
+STAKE_AMT=100
+BET_AMT=1
+WIN=1
+LOOSE=0
+
+random=$((RANDOM%2))
+echo "Value generated is:"$random
+
+if [[ $random -eq $WIN ]]
+then
+	echo "You Win $"$BET_AMT
+else
+	echo "You loose $"$BET_AMT
+fi
 
