3107e7a46ac26d14c2288abc4168118e80cf23ba
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-11 12:16:24 +0530

    [Vaidehi]ADD:Calculative Gambler

--- GambleSimulation.sh
+++ GambleSimulation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+
 
 #CONSTANTS FOR THE PROGRAMM
 STAKE_AMT=100
@@ -16,45 +16,46 @@ echo "percentage:"$percent
 max=$((percent+STAKE_AMT))
 min=$((STAKE_AMT-percent))
 
-#chance=0;
-#while [[ $total_amt -lt $max && $chance -lt $min ]]
-#do
-#((chance++))
-#betCheck=$(($RANDOM%2));
-#case $betCheck in
-#  $WIN)
-#      BET_AMT=1
-#         ;;
-#  $LOOSE)
-#       BET_AMT=1
-#        ;;
-#esac
-#total_amt=$(( $total_amt + $BET_AMT ));
 
-#done
-
-#echo "total amount is:" $total_amt
-
-
-
-
-
-chance=0
-while [[ $totalbet_win_amt -lt $max && $totalbet_loose_amt -gt $min ]]
+while [[ $totalbet_win_amt -lt $max && $totalbet_loose_amt -lt $min ]]
 do
-	((chance++))
-	random=$((RANDOM%2))
-	if [[ $random -eq $WIN ]]
-	then
-		echo "You Win $"$BET_AMT
-#		totalbet_win_amt=$(( $totalbet_win_amt + $BTE_AMT ))
-	else
-		echo "You loose $"$BET_AMT
-#		totalbet_loose_amt=$(( $totalbet_loose_amt + $BET_AMT))
-	fi
-totalbet_win_amt=$(( $totalbet_win_amt + $BTE_AMT ))
-totalbet_loose_amt=$(( $totalbet_loose_amt + $BET_AMT))
+betCheck=$(($RANDOM%2));
+case $betCheck in
+  $WIN)
+      BET_AMT=1  totalbet_win_amt=$(($totalbet_win_amt + 1))
+         ;;
+  $LOOSE)
+       BET_AMT=1  totalbet_loose_amt=$(($totalbet_loose_amt + 1))
+        ;;
+esac
+#total_amt=$(( $total_amt + $BET_AMT ));
+echo "winning amt is:" $totalbet_win_amt
+echo "Loosing amount is:"$totalbet_loose_amt
 done
-echo "Total winning amount is:" $totalbet_win_amt
-echo "Total loosing amount is:" $totalbet_loose_amt
+echo "Total winning amount:" $(($totalbet_win_amt + $STAKE_AMT))
+echo "total loosing amount is:" $(($totalbet_loose_amt + $STAKE_AMT))
+
+
+
+
+
+
+#chance=0
+#while [[ $totalbet_win_amt -lt $max && $totalbet_loose_amt -gt $min ]]
+#do
+#	((chance++))
+#	random=$((RANDOM%2))
+#	if [[ $random -eq $WIN ]]
+#	then
+#		echo "You Win $"$BET_AMT
+#		totalbet_win_amt=$(( $totalbet_win_amt + $BTE_AMT ))
+#	else
+#		echo "You loose $"$BET_AMT
+#		totalbet_loose_amt=$(( $totalbet_loose_amt + $BET_AMT))
+#	fi
+#totalbet_win_amt=$(( $totalbet_win_amt + $BTE_AMT ))
+#totalbet_loose_amt=$(( $totalbet_loose_amt + $BET_AMT))
+#done
+#echo "Total winning amount is:" $totalbet_win_amt
+#echo "Total loosing amount is:" $totalbet_loose_amt
 
