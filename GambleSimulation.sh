4ca093ec60eb493b7eb508d62bd802438e664c05
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-11 21:19:43 +0530

    [Vaidehi]ADD:Calculating for twenty days

--- GambleSimulation.sh
+++ GambleSimulation.sh
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
 
-
-#CONSTANTS FOR THE PROGRAMM
-stakeAMT=100
-betAMT=1
-win=1
-loose=0
-
-#VARIABLES
-totalbetWinAmt=0
-totalbetLooseAmt=0
-=======
 #CONSTANTS FOR THE PROGRAMM
 stakeAMT=100
 betAMT=1
 totalAmt=$((stakeAMT))
 allDaysCOllection=0
->>>>>>> UC4_calculatingAmountFor20Days
+
 
 percent=$(((stakeAMT*50)/100))
 echo "percentage:"$percent
@@ -24,26 +12,7 @@ echo "percentage:"$percent
 max=$((percent+stakeAMT))
 min=$((stakeAMT-percent))
 
-<<<<<<< HEAD
 
-while [[ $totalbetWinAmt -lt $max && $totalbetLooseAmt -lt $min ]]
-do
-betCheck=$(($RANDOM%2));
-case $betCheck in
-  $win)
-      betAMT=1  totalbetWinAmt=$(( $totalbetWinAmt + 1 ))
-         ;;
-  $loose)
-       betAMT=1  totalbetLooseAmt=$(( $totalbetLooseAmt + 1 ))
-        ;;
-esac
-echo "winning amt is:" $totalbetWinAmt
-echo "Loosing amount is:"$totalbetLooseAmt
-done
-echo "Total winning amount:" $(($totalbetWinAmt + $stakeAMT))
-echo "total loosing amount is:" $(($totalbetLooseAmt + $stakeAMT))
-
-=======
 declare -A collectionPerDay
 totalDays=20
 stakeForTwentyDays=$(($stakeAMT*$totalDays))
@@ -77,4 +46,4 @@ else
 	looseBy=`expr $stakeForTwentyDays - $allDaysCollection`
 	echo "Gambler loose by:" $looseBy
 fi
->>>>>>> UC4_calculatingAmountFor20Days
+
