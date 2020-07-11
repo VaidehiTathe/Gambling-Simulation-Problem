e41f78bd4b83331d57fe02c612cb06efd41d3cc1
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-11 16:48:58 +0530

    [Vaidehi]ADD:calculating total amount

--- GambleSimulation.sh
+++ GambleSimulation.sh
@@ -1,15 +1,55 @@
-#!/bin/bash -x
-STAKE_AMT=100
-BET_AMT=1
-WIN=1
-LOOSE=0
+#CONSTANTS FOR THE PROGRAMM
+stakeAMT=100
+betAMT=1
+win=1
+loose=0
 
-random=$((RANDOM%2))
-echo "Value generated is:"$random
+#VARIABLES
+totalAmt=$((stakeAMT))
+totalDays=20
+winAMT=0
+looseAMT=0
 
-if [[ $random -eq $WIN ]]
-then
-	echo "You Win $"$BET_AMT
-else
-	echo "You loose $"$BET_AMT
-fi
+percent=$(((stakeAMT*50)/100))
+echo "percentage:"$percent
+
+max=$((percent+stakeAMT))
+min=$((stakeAMT-percent))
+
+resign()
+{
+	while [[ $totalAmt -gt $min && $totalAmt -lt $max ]]
+	do
+		betCheck=$(($RANDOM%2));
+		case $betCheck in
+			$win)
+      			betAMT=1  totalAmt=$(( $totalAmt + 1 ))
+         			;;
+  			$loose)
+		       	betAMT=1  totalAmt=$(( $totalAmt + 1 ))
+        			;;
+		esac
+	done
+}
+resign
+
+echo "Total amount is:" $totalAmt
+
+calculateTotalAmount()
+{
+for (( day=1; day<=$totalDays; day++ ))
+do 
+	totalAMT=$stakeAMT
+	if [ $totalAmt -gt $stakeAMT ]
+	then
+		winAMt=$(( $totalAmt-$stakeAMT ))
+#		echo "total amt is:"$totalAMT
+		totalwinAMT=$(( $totalwinAMT + $winAMT))
+	else
+		looseAMT=$(( $stakeAMT-$totalAMT ))
+	fi
+done
+}
+calculateTotalAmount
+echo "Winning amount is:" $totalwinAMT
+echo "Loosing amount is:" $looseAMT
