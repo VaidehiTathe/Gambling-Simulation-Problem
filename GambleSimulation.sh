61336b1f71ae3a069a84d0e03633030486a9878b
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-15 11:06:56 +0530

    [Vaidehi]REFACTOR:added for loop to get luckiest day

--- GambleSimulation.sh
+++ GambleSimulation.sh
@@ -1,24 +1,20 @@
 #!/bin/bash +x
 
 #CONSTANTS FOR THE PROGRAMM
-stakeAMT=100
-betAMT=1
-totalAmt=$((stakeAMT))
-prevDayCollection=$((stakeAMT))
+STAKEAMT=100
+BETAMT=1
+perDayCollection=$((STAKEAMT))
 
 
 declare -A collectionPerDay
 counterForCollectionPerDay=1
-totalDays=15
-stakeForTwentyDays=$(($stakeAMT*$totalDays))
+totalDays=5
+stakeForTwentyDays=$(($STAKEAMT*$totalDays))
 
 #VARIABLES FOR FUNCTIONS daywonloose
 countOfDaysWin=0
 countOfDaysLoose=0
-declare -a luckyDays
-counterOfLuckyDays=1
-declare -a unluckyDays
-counterOfUnluckyDays=0
+
 
 function play()
 {
@@ -26,25 +22,24 @@ for(( day=1; day<=$totalDays; day++ ))
 do
 
 	
-	percent=$(((prevDayCollection*50)/100))
-	echo "percentage:"$percent
+	percent=$(((perDayCollection*50)/100))
 
-	max=$((percent+prevDayCollection))
-	min=$((prevDayCollection-percent))
+	max=$((percent+perDayCollection))
+	min=$((perDayCollection-percent))
 
-        while [[ $prevDayCollection -le $max &&  $prevDayCollection -ge $min ]]
+        while [[ $perDayCollection -le $max &&  $perDayCollection -ge $min ]]
         do
                 betCheck=$(($RANDOM%2));
                 if [[ $betCheck -eq 1 ]]
                 then
-                        prevDayCollection=$(( $prevDayCollection + $betAMT ))
+                        perDayCollection=$(( $perDayCollection + $BETAMT ))
                 else
-                        prevDayCollection=$(( $prevDayCollection - $betAMT ))
+                        perDayCollection=$(( $perDayCollection - $BETAMT ))
                 fi
         done
-        collectionPerDay[$day]=$((prevDayCollection))
-	prevDayCollection=$(($prevDayCollection)) 
-	echo "collection for day $day is:"$prevDayCollection
+        collectionPerDay[$day]=$((perDayCollection))
+	perDayCollection=$(($perDayCollection)) 
+	echo "collection for day $day is:"$perDayCollection
 done
 daysWonLoose
 }
@@ -65,19 +60,7 @@ function choice()
 
 function luckyUnluckyDays()
 {
-	for days in ${!collectionPerDay[@]}
-	do
-		for money in ${collectionPerDay[@]}
-		do
-			luckyDays[((counterOfLuckyDays++))]=$money
-			# luckyDays([$days])=$money
-		done
-		break
-	done
-	echo "lucky day array is:${!luckyDays[@]}"
-	echo "lucky money array is:${luckyDays[@]}"
 	size=${#collectionPerDay[@]}
-	echo "size of array is $size"
 	highestAmount=0;
         for(( num=1;num<=size;num++ ))
  	do
@@ -90,6 +73,18 @@ function luckyUnluckyDays()
 	echo "The luckiest day is: "$luck
 	echo "The largest number is: "$highestAmount
 
+        lowestAmount=0;
+        for(( num=1;num<=size;num++ ))
+        do
+                if [[ collectionPerDay[$num] -le lowestAmount ]]
+                then
+                        lowestAmount=$((collectionPerDay[$num]))
+                        unLuck=$num
+                fi
+        done
+        echo "The unluckiest day is: "$unLuck
+        echo "The money that get on luckiest day is: "$lowestAmount
+
 }
 
 
@@ -99,9 +94,8 @@ function luckyUnluckyDays()
 totalAmtForAllDays=0
 function daysWonLoose()
 {
-        #echo "All day Collection is:"$allDaysCollection
-#        echo "Collection per day for month is:" ${collectionPerDay[@]}
 
+	luckyUnluckyDays
         for i in "${!collectionPerDay[@]}"
         do
 		totalAmtForAllDays=$(($totalAmtForAllDays+${collectionPerDay[$i]}))
@@ -112,11 +106,12 @@ function daysWonLoose()
 	then
 		winBy=`expr $totalAmtForAllDays - $stakeForTwentyDays`
 		echo "Gambler win by:"$winBy
-		#choice
+		choice
 	else
 		looseBy=`expr $stakeForTwentyDays - $totalAmtForAllDays`
 		echo "Gambler loose by:" $looseBy
 	fi
-	luckyUnluckyDays
+
+
 }
 play
